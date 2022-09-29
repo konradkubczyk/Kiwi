a85f07bd049d6fea6b7006d7e4becb49e7a07de6
Konrad Kubczyk <konrad@kubczyk.com>
2022-09-30 01:47:21 +0200

    Indentation change

--- routes/index.js
+++ routes/index.js
@@ -5,51 +5,51 @@ const Schedule = require('../controllers/schedule');
 
 // GET home page
 router.get('/', function (req, res, next) {
-  const appDomain = req.get('host');
+    const appDomain = req.get('host');
 
-  res.render('index', { appDomain });
+    res.render('index', { appDomain });
 });
 
 // GET /* (all other routes)
 router.get('/*', async function (req, res, next) {
-  
-  // Get schedule URL from request and while changing protocol to https
-  let scheduleURL = 'https://' + req.originalUrl.slice(1).replace(/(^\w+:|^)\/\//, '');
 
-  // List of supported schedule sources' domains
-  const allowedDomains = [
-    'planzajec.uek.krakow.pl'
-  ];
+    // Get schedule URL from request and while changing protocol to https
+    let scheduleURL = 'https://' + req.originalUrl.slice(1).replace(/(^\w+:|^)\/\//, '');
 
-  // Check if schedule URL is from supported domain
-  try {
-    const domain = new URL(scheduleURL).hostname;
+    // List of supported schedule sources' domains
+    const allowedDomains = [
+        'planzajec.uek.krakow.pl'
+    ];
 
-    if (!allowedDomains.includes(domain)) {
-      return res.status(400).send('Invalid domain, allowed domains: ' + allowedDomains.join(', '));
+    // Check if schedule URL is from supported domain
+    try {
+        const domain = new URL(scheduleURL).hostname;
+
+        if (!allowedDomains.includes(domain)) {
+            return res.status(400).send('Invalid domain, allowed domains: ' + allowedDomains.join(', '));
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(400).send('Invalid URL');
     }
-  } catch (error) {
-    console.error(error);
-    return res.status(400).send('Invalid URL');
-  }
 
-  // Create schedule object
-  const schedule = new Schedule(scheduleURL);
+    // Create schedule object
+    const schedule = new Schedule(scheduleURL);
 
-  // Get calendar data in ICS format
-  try {
-    const ics = await schedule.getICS();
+    // Get calendar data in ICS format
+    try {
+        const ics = await schedule.getICS();
 
-    res.set('Content-Type', 'text/calendar');
+        res.set('Content-Type', 'text/calendar');
 
-    // Set filename for calendar file
-    res.attachment('schedule.ics');
+        // Set filename for calendar file
+        res.attachment('schedule.ics');
 
-    res.send(ics);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send('Error while fetching data and generating calendar');
-  }
+        res.send(ics);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Error while fetching data and generating calendar');
+    }
 });
 
 module.exports = router;
