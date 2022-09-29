0e6a8cb273e0e2268c65d7ec9a79bece3a95747f
Konrad Kubczyk <konrad@kubczyk.com>
2022-09-29 23:52:30 +0200

    Add schedule transcriber

---
+++ controllers/schedule.js
@@ -0,0 +1,101 @@
+const axios = require('axios');
+const https = require('https');
+const xml2js = require('xml2js');
+const ical = require('ical-generator');
+
+class Schedule {
+  constructor(scheduleURL) {
+    this.scheduleURL = scheduleURL + '&xml';
+  }
+
+  /**
+   * Gets XML data from schedule URL and returns it as JSON
+   * @returns {Promise} Calendar data in JSON format
+   */
+  async #getData() {
+    const agent = axios.create({
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false
+      })
+    });
+
+    let response;
+    try {
+      response = await agent.get(
+        this.scheduleURL,
+        {
+          headers: {
+            'Content-Type': 'application/xml'
+          }
+        }
+      );
+    } catch (error) {
+      console.error(error);
+      throw new Error('Error while fetching data');
+    }
+
+    const scheduleXML = response.data;
+
+    try {
+      const parser = new xml2js.Parser();
+      const scheduleJSON = await parser.parseStringPromise(scheduleXML);
+      return scheduleJSON;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Error while parsing data from response');
+    }
+  }
+
+  /**
+   * Gets calendar data from schedule URL and returns it as ICS
+   * @returns {Promise} Calendar data in ICS format
+   */
+  async getICS() {
+    const scheduleJSON = await this.#getData();
+
+    let events;
+    try {
+      events = scheduleJSON['plan-zajec']['zajecia'];
+    } catch (error) {
+      console.error(error);
+      throw new Error('Error while trying to get events from parsed data');
+    }
+
+    try {
+      const calendar = new ical({
+        name: 'Plan zajęć ' + scheduleJSON['plan-zajec']['$']['nazwa'],
+        timezone: 'Europe/Warsaw',
+        ttl: 60 * 60 * 12,
+        prodId: {
+          company: 'UEK',
+          product: 'Plan zajęć',
+          language: 'PL'
+        },
+        source: this.scheduleURL
+      });
+
+      for (const event of events) {
+        calendar.createEvent({
+          summary: event['przedmiot'][0],
+          start: new Date(
+            event['termin'][0] + 'T' + event['od-godz'][0] + ':00'
+          ),
+          end: new Date(
+            event['termin'][0] + 'T' + event['do-godz'][0].split(' ')[0] + ':00'
+          ),
+          location: event['sala'][0],
+          description: event['typ'][0].charAt(0).toUpperCase() + event['typ'][0].slice(1) + '\n' + event['nauczyciel'].map(organizer => organizer['_']).join(', ')
+        });
+      }
+
+      const scheduleICS = calendar.toString();
+
+      return scheduleICS;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Error while creating ICS data');
+    }
+  }
+}
+
+module.exports = Schedule;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const Schedule = require('../controllers/schedule');
+
+// GET home page
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Hermes' });
+});
+
+// GET /* (all other routes)
+router.get('/*', async function (req, res, next) {
+  
+  // Get schedule URL from request and while changing protocol to https
+  let scheduleURL = 'https://' + req.originalUrl.slice(1).replace(/(^\w+:|^)\/\//, '');
+
+  // List of supported schedule sources' domains
+  const allowedDomains = [
+    'planzajec.uek.krakow.pl'
+  ];
+
+  // Check if schedule URL is from supported domain
+  try {
+    const domain = new URL(scheduleURL).hostname;
+
+    if (!allowedDomains.includes(domain)) {
+      return res.status(400).send('Invalid domain, allowed domains: ' + allowedDomains.join(', '));
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send('Invalid URL');
+  }
+
+  // Create schedule object
+  const schedule = new Schedule(scheduleURL);
+
+  // Get calendar data in ICS format
+  try {
+    const ics = await schedule.getICS();
+
+    res.set('Content-Type', 'text/calendar');
+
+    // Set filename for calendar file
+    res.attachment('schedule.ics');
+
+    res.send(ics);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Error while fetching data and generating calendar');
+  }
 });
 
 module.exports = router;
