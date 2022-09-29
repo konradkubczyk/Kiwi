ea87d4f5ee68606ff75c838880a8704e0b89382a
Konrad Kubczyk <konrad@kubczyk.com>
2022-09-30 00:33:43 +0200

    Add Bootstrap

--- app.js
+++ app.js
@@ -4,36 +4,43 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+// Import routes
 const licensesRouter = require('./routes/licenses');
 const indexRouter = require('./routes/index');
 
+// Create Express app
 const app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// Static files
+app.use('/static', express.static(path.join(__dirname, '/public')));
+app.use('/static/bootstrap', express.static(path.join(__dirname, '/node_modules/bootstrap/dist')));
+
+// Routes
 app.use('/licenses', licensesRouter);
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error', { title: 'Error' });
 });
