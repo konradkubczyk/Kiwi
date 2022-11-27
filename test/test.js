876b174e57c2644f993a7c721385ba0ad8ae0368
Konrad Kubczyk <konrad@kubczyk.com>
2022-11-27 01:09:12 +0100

    Add tests

--- test/test.js
+++ test/test.js
@@ -5,6 +5,17 @@ describe('App', function() {
   it('has the default page', function(done) {
     request(app)
       .get('/')
-      .expect(/Welcome to Express/, done);
+      .expect(/Welcome to Kiwi/, done);
   });
-}); 
+  it('has the licenses page', function(done) {
+    request(app)
+      .get('/licenses')
+      .expect(/express/, done);
+  });
+  it('generates a schedule', function(done) {
+    request(app)
+      .get('/https://planzajec.uek.krakow.pl/index.php?typ=G&id=190181&okres=1')
+      .expect('Content-Type', /text\/calendar/)
+      .expect(/BEGIN:VCALENDAR/, done);
+  });
+});
